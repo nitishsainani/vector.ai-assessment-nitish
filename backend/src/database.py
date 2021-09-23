a8d7b713210ad49023e7d18f945bdf638c349520
nitishsainani <nitish.sainani@gmail.com>
2021-09-24 00:13:57 +0530

    changed db_name

--- backend/src/database.py
+++ backend/src/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:mysuperpassword@localhost/youtube"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:mysuperpassword@localhost/vector_ai"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
