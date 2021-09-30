9961eb80ddfc079e54ff57c8d1efcbe3cb9db09f
nitishsainani <nitish.sainani@gmail.com>
2021-09-30 20:39:33 +0530

    added Loading spinner while images load.

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,9 +1,13 @@
 import {useEffect, useState} from "react";
 import {Grid} from "@mui/material";
 import {BackendService} from '../services';
+import Loader from "react-loader-spinner";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+
 
 function HomePage() {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     BackendService.BoardService.getItems().then(setData);
@@ -26,7 +30,14 @@ function HomePage() {
           return (
             <Grid key={item.id} item xs={4} style={{textAlign: "center"}}>
               <p>{item.title}</p>
-              <img src={"https://picsum.photos/300/300"} alt={"image"}/>
+              {loading && <Loader
+                type="Rings"
+                color="#00BFFF"
+                height={300}
+                width={300}
+                timeout={3000}
+              />}
+              <img onLoad={() => setLoading(false)} src={"https://picsum.photos/300/300"} alt={"board"}/>
             </Grid>
           );
         })}
